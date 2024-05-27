bc7c8bf8e06ef8e80c76048ad7c793796d3cea0c
Dhruv <dhruvgop@gmail.com>
2024-05-28 02:06:46 +0530

    added energy aware metrics and cleaned up code

--- main.py
+++ main.py
@@ -68,6 +68,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--dir', type=str, default='datasets/')
 parser.add_argument('--sortpar', type=str, default='weight', help='weight or rho')
 parser.add_argument('--cq', type=str, default='ch', help='classical or quantum or cheating')
+parser.add_argument('--output_dir', type=str, default='outputs/overlap/')
 
 args = parser.parse_args()
 
@@ -75,10 +76,11 @@ args = parser.parse_args()
 data_dir = args.dir
 sortpar = args.sortpar
 cq = args.cq
-output_dir = 'output_circles_num/'
+output_dir = args.output_dir
 
 os.makedirs(output_dir, exist_ok=True)
 dists = [0,20,25,30,35,40,50,60,70,80,100,120,140]
+
 iters = 30
 h_scores = np.zeros((len(dists), iters))
 c_scores = np.zeros((len(dists), iters))
@@ -86,7 +88,7 @@ v_scores = np.zeros((len(dists), iters))
 
 # dists=[100,200]
 it = time.time()
-prefactors = [1,2,5,10,20]
+prefactors = [1,2,5,10]
 ans = np.zeros((h_scores.shape[0], h_scores.shape[1], len(prefactors)))
 for pfi, prefactor_factor in enumerate(prefactors):
     if len(glob.glob(output_dir+'h_score_'+str(pfi)+'.npy')) == 0:
@@ -109,7 +111,7 @@ for pfi, prefactor_factor in enumerate(prefactors):
 
                 means = [[-dists[dist_index],0],[dists[dist_index],0]]
                 covs = [cova]*2
-                prefactor = [1e5, 1e5]
+                prefactor = [500*2*np.pi*900, 500*2*np.pi*900]
                 n_clusters = len(means)
                 df = pd.DataFrame(columns=['x','y','clusterId'])
                 for i in range(n_clusters):
@@ -118,7 +120,7 @@ for pfi, prefactor_factor in enumerate(prefactors):
                     df1['weight'] = prefactor[i]*multivariate_normal.pdf(df1[['x', 'y']].values, mean=means[i], cov=covs[i])
                     df = pd.concat([df,df1])
                 noise = np.random.uniform(-250,250, (n_noise,2))
-                n = pd.DataFrame(columns=['x','y','clusterId', 'weight'], data=np.c_[noise,0*np.ones((n_noise)), 2*np.random.random(n_noise)])
+                n = pd.DataFrame(columns=['x','y','clusterId', 'weight'], data=np.c_[noise,0*np.ones((n_noise)), np.random.random(n_noise)])
                 df = pd.concat([df,n])
                 df['layer']=np.zeros(len(df))
 
@@ -163,16 +165,7 @@ for pfi, prefactor_factor in enumerate(prefactors):
                 layer = selected_data['layer'].values
                 weight = selected_data['weight'].values
 
-                # trueDensity = selected_data['rho'].values
-                # trueNh = selected_data['nh'].values.astype(int)
-                # trueDelta = selected_data['delta'].values
-                # trueisSeed = selected_data['isSeed'].values
                 trueClusterId = selected_data['clusterId'].values
-
-
-                # computedNH = selected_data['NH'].values.astype(int)
-                # computedClusterNumbers = selected_data['ClusterNumbers'].values
-                # computedisOutlier = selected_data['isOutlier'].values
 
                 # Create dataframe
                 # dataset = pd.DataFrame(np.array([x,y,layer,weight, trueNh, trueDensity, computedNH, computedClusterNumbers, computedisOutlier]).T, columns=['x','y','layer','weight', 'nh_old', 'rho', 'NH', 'ClusterNumbers', 'isOutlier'])
@@ -185,37 +178,22 @@ for pfi, prefactor_factor in enumerate(prefactors):
                 for idx in uniqueTileIdx:
                     tileDict[idx] = np.where(tilesList==idx)[0]
 
-                # dataset['tileIdx'] = tilesList
-                # print(tileDict)
-                # s = dec_to_qubit(3, dataset)
-                # print(s)
-                # print(qubit_to_dec(s, dataset))
-
                 # TODO: Check if localdensities agree
                 if cq == 'q':
                     ld, dataset_ld = calculateLocalDensity_classic_mod(dataset, tileDict, dc)
                 elif cq == 'c':
                     ld, dataset_ld = calculateLocalDensity_classic(dataset, tileDict, dc)
-                # elif cq == 'ch':
-                #     localDensities = trueDensity
-                # dataset['rho'] = localDensities
-                # print(dataset.head())
 
                 # print('Density:', all(localDensities==trueDensity))
-                # dataset_ld.to_csv('datasets/dataset_generated_ld_c.csv')
+                dataset_ld.to_csv('datasets/dataset_generated_ld_overlap.csv')
+                print('Local density calculated')
 
                 NH, dataset = calculateNearestHigher_classic_mod(dataset_ld, tileDict, outlierDeltaFactor*dc, delC, phoC, delM)
 
-                # NH, dataset = calculateNearestHigher_classic_mod_hard(dataset_ld, tileDict, outlierDeltaFactor*dc, delC, phoC, delM)
-                # NH, dataset = calculateNearestHigher_classic_mod(dataset, tileDict, outlierDeltaFactor*dc, delC, phoC, delM)    
-                # dataset.to_csv('datasets/dataset1_correct_generated_c.csv')
                 dataset1 = findAndAssign_clusters_classic_fast(dataset, tileDict, delM)
 
-                # dataset1.to_csv('D1_correct_generated_c.csv')
-                
-
-                h_score,c_score,v_score = homogeneity_completeness_v_measure(dataset1['clusterId'].values, dataset1['ClusterNumbers'].values)
-                #print(h_score, c_score, v_score)
+                h_score,c_score,v_score = energy_aware_homogeneity_completeness_v_measure(dataset1['clusterId'].values, dataset1['ClusterNumbers'].values, energy=dataset1['weight'].values)
+                print(h_score, c_score, v_score)
                 h_scores[dist_index, iter] = h_score
                 c_scores[dist_index, iter] = c_score
                 v_scores[dist_index, iter] = v_score
@@ -225,84 +203,33 @@ for pfi, prefactor_factor in enumerate(prefactors):
                 plt.text(0,75,'score: ' + str(round(h_score, 2)))
                 plt.ylim(-100,100)
                 plt.xlim(-100,100)
-                plt.savefig(output_dir+'/computed_clusters_'+str(dists[dist_index])+'_'+str(pfi)+'.png')
+                plt.text(-230,-230, '$\mathcal{F}_H= $'+ str(round(h_score,2)))
+                plt.savefig(output_dir+'/computed_clusters_'+str(dists[dist_index])+'_'+str(pfi)+'.svg')
                 plt.close()
 
                 sns.scatterplot(data=dataset1, x="x", y="y", hue="clusterId", palette="deep").set_title('True clusters')
                 plt.legend([],[], frameon=False)
-                plt.savefig(output_dir + 'true_clusters_'+str(dists[dist_index])+'_'+str(pfi)+'.png')
+                plt.text(-230,-230, '$\mathcal{F}_H= $'+ str(round(h_score,2)))
+                plt.savefig(output_dir + 'true_clusters_'+str(dists[dist_index])+'_'+str(pfi)+'.svg')
                 plt.close()
 
-            # print(h_scores)
-
-        # print(h_scores)
-        # print(c_scores)
-    #     # print(v_scores)
-    # np.save('h_scores.npy', h_scores)
-    # np.save('c_scores.npy', c_scores)
-    # np.save('v_scores.npy', v_scores)
-
-    # h_scores = np.load('h_scores.npy')
-    # c_scores = np.load('c_scores.npy')
-    # v_scores = np.load('v_scores.npy')
         print(h_scores.shape)
-    # print(c_scores.shape)
-    # print(v_scores.shape)
-        # plt.plot(dists, np.mean(h_scores, axis=1), label='homogeneity_'+str((pfi)))
-        # plt.fill_between(dists, np.mean(h_scores, axis=1)-np.std(h_scores, axis=1), np.mean(h_scores, axis=1)+np.std(h_scores, axis=1), alpha=0.2)
-        # print(h_scores.shape, a.shape)
         np.save(output_dir+'h_score_'+str(pfi)+'.npy', np.array(h_scores))
     else:
+        print('Loading from file')
         h_scores = np.load(output_dir+'h_score_'+str(pfi)+'.npy')
     plt.plot(dists, np.mean(h_scores, axis=1), label='homogeneity_'+str((pfi)))
     plt.fill_between(dists, np.mean(h_scores, axis=1)-np.std(h_scores, axis=1), np.mean(h_scores, axis=1)+np.std(h_scores, axis=1), alpha=0.2)
     # plt.savefig(output_dir+'homogeneity_varied_energy_'+str(pfi)+'.png')
     ans[:,:,pfi] = np.array(h_scores)
     
-plt.xlabel('Distance between clusters')
-plt.ylabel('Homogeneity')
-plt.legend(prefactors)
+plt.xlabel('$N_{N}/N_{C}$')
+plt.ylabel('$\mathcal{F}$')
+plt.legend(['$\frac{N_1}{N_2}=$' + str(i) for i in prefactors])
 np.save(output_dir+'homogeneities_varied_energy.npy', ans)
 # plt.text(100,0, h_score)
 plt.savefig(output_dir+'homogeneity_varied_energy.png')
 plt.close()
 
-# plt.close()
-# plt.plot(dists, np.mean(c_scores, axis=1), label='completeness')
-# plt.fill_between(dists, np.mean(c_scores,axis=1)-np.std(c_scores, axis=1), np.mean(c_scores, axis=1)+np.std(c_scores, axis=1), alpha=0.2)
-# plt.savefig('completeness.png')
-# plt.close()
-
-# plt.plot(dists, v_scores, label='v_measure')
-# plt.fill_between(dists, np.mean(v_scores, axis=1)-np.std(v_scores, axis=1), np.mean(v_scores, axis=1)+np.std(v_scores, axis=1), alpha=0.2)
-# plt.savefig('v_measure.png')
-# plt.close()
-
-
-
 ft = time.time()
 print('Time taken:', ft-it)
-
-    # wandb.log({
-    #     'homogeneity': h, 
-    #     'completeness': c,
-    #     'v_measure': v,
-    #     })
-# print(NH)
-# print(dataset1.head())
-# dataset1.to_csv('dataset1.csv')
-# print(NH==trueNh)
-# print(pauli_gen("I", 0, 2))
-
-
-####### TODOLIST
-#     # - calculate local density
-    # write quantum version of the function using one of two methods (grover -> one-point state, grover -> all-points state)
-
-    # - find nearest higher
-    # use grover -> one-point state (good because nearest higher is unique)       
-
-# wandb.agent(sweep_id, function=main)
-# fig = plt.figure()
-# plt.scatter(allX,allY)
-# plt.savefig('scatter_plot.pdf')
--- main_noise.py
+++ main_noise.py
@@ -68,7 +68,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--dir', type=str, default='datasets/')
 parser.add_argument('--sortpar', type=str, default='weight', help='weight or rho')
 parser.add_argument('--cq', type=str, default='ch', help='classical or quantum or cheating')
-parser.add_argument('--output_dir', type=str, default='output/noise_sigma/', help='output directory')
+parser.add_argument('--output_dir', type=str, default='outputs/noise_sigma/', help='output directory')
 
 
 args = parser.parse_args()
@@ -81,39 +81,23 @@ output_dir = args.output_dir
 
 os.makedirs(output_dir, exist_ok=True)
 n_samples = [750]
-
+plt.rc('text', usetex=True)
 
 noise_sizes = [0,50,100,150,200,250,300,350,400,450,500,550,600,650,700,750]
-# noise_sizes = [250,300,350,400,450]
-# dists=[100,200]
 it = time.time()
 iters = 30
-sigmas = [10000]#[1,100,1000,10000]
-# sigmas = [10,1000]
+sigmas = [3, 10, 32]
+
 h_scores = np.zeros((len(noise_sizes), iters, len(sigmas)))
-# c_scores = np.zeros((len(noise_sizes), iters))
-# v_scores = np.zeros((len(noise_sizes), iters))
 for n_sigma, sigma in enumerate(sigmas):
-    # print('Length is: ', len(glob.glob(data_dir+'h_scores_noise_sigma_' + str(sigma) + '.npy')))
     if len(glob.glob(output_dir+'h_scores_noise_sigma_' + str(sigma) + '.npy'))==0:
         for iter in range(iters):
             for noise_index in range(len(noise_sizes)):
-
-                
-                # wandb.run=None
-                # run=wandb.init()
-                cova = np.array([[sigma,0],[0,sigma]])
-
-                # print(wandb.config)
-
-                # dist  =  wandb.config.dist
-
-                
+                cova = np.array([[sigma**2,0],[0,sigma**2]])
                 n_noise = noise_sizes[noise_index]
-
                 means = [[0,0]]
                 covs = [cova]
-                prefactor = [1e5]*2
+                prefactor = [2*np.pi*np.sqrt(np.linalg.det(covs))*500]*2
                 n_clusters = len(means)
                 df = pd.DataFrame(columns=['x','y','clusterId'])
                 for i in range(n_clusters):
@@ -122,25 +106,19 @@ for n_sigma, sigma in enumerate(sigmas):
                     df1['weight'] = prefactor[i]*multivariate_normal.pdf(df1[['x', 'y']].values, mean=means[i], cov=covs[i])
                     df = pd.concat([df,df1])
                 noise = np.random.uniform(-250,250, (n_noise,2))
-                n = pd.DataFrame(columns=['x','y','clusterId', 'weight'], data=np.c_[noise,0*np.ones((n_noise)), 2*np.random.random(n_noise)])
+                n = pd.DataFrame(columns=['x','y','clusterId', 'weight'], data=np.c_[noise,0*np.ones((n_noise)), np.random.random(n_noise)])
                 df = pd.concat([df,n])
                 df['layer']=np.zeros(len(df))
 
                 df.to_csv(data_dir+ 'gen_data_'+str(noise_sizes[noise_index])+'_noise.csv')
-
-                
-
-
-
                 # Import the data for QLUE
                 qlue_data = pd.read_csv(data_dir+ 'gen_data_'+str(noise_sizes[noise_index])+'_noise.csv')
-                # qlue_data = pd.read_csv(data_dir+ "dataset1.csv")
 
                 # Define variables and parameters needed by the algo
                 outlierDeltaFactor = 2
 
-                dc = 2*np.sqrt(sigma)/3
-                rhoc = 200
+                dc = 20
+                rhoc = 25
                 delM = outlierDeltaFactor*dc
                 # delM = dc
 
@@ -167,19 +145,9 @@ for n_sigma, sigma in enumerate(sigmas):
                 layer = selected_data['layer'].values
                 weight = selected_data['weight'].values
 
-                # trueDensity = selected_data['rho'].values
-                # trueNh = selected_data['nh'].values.astype(int)
-                # trueDelta = selected_data['delta'].values
-                # trueisSeed = selected_data['isSeed'].values
                 trueClusterId = selected_data['clusterId'].values
 
-
-                # computedNH = selected_data['NH'].values.astype(int)
-                # computedClusterNumbers = selected_data['ClusterNumbers'].values
-                # computedisOutlier = selected_data['isOutlier'].values
-
                 # Create dataframe
-                # dataset = pd.DataFrame(np.array([x,y,layer,weight, trueNh, trueDensity, computedNH, computedClusterNumbers, computedisOutlier]).T, columns=['x','y','layer','weight', 'nh_old', 'rho', 'NH', 'ClusterNumbers', 'isOutlier'])
                 dataset = pd.DataFrame(np.array([x,y,layer,weight, trueClusterId]).T, columns=['x','y','layer','weight', 'clusterId'])
                 # Calculate tile indices and fill tiles as a dictionary
                 tilesList = [getGlobalBin(x[k],y[k]) for k in range(len(x))]
@@ -189,122 +157,55 @@ for n_sigma, sigma in enumerate(sigmas):
                 for idx in uniqueTileIdx:
                     tileDict[idx] = np.where(tilesList==idx)[0]
 
-                # dataset['tileIdx'] = tilesList
-                # print(tileDict)
-                # s = dec_to_qubit(3, dataset)
-                # print(s)
-                # print(qubit_to_dec(s, dataset))
-
                 # TODO: Check if localdensities agree
                 if cq == 'q':
                     ld, dataset_ld = calculateLocalDensity_classic_mod(dataset, tileDict, dc)
                 elif cq == 'c':
                     ld, dataset_ld = calculateLocalDensity_classic(dataset, tileDict, dc)
-                # elif cq == 'ch':
-                #     localDensities = trueDensity
-                # dataset['rho'] = localDensities
-                # print(dataset.head())
 
-                # print('Density:', all(localDensities==trueDensity))
                 dataset_ld.to_csv('datasets/dataset_generated_ld_c_noise.csv')
+                print('LDs computed')
 
                 NH, dataset = calculateNearestHigher_classic_mod(dataset_ld, tileDict, outlierDeltaFactor*dc, delC, phoC, delM)
 
-                # NH, dataset = calculateNearestHigher_classic_mod_hard(dataset_ld, tileDict, outlierDeltaFactor*dc, delC, phoC, delM)
-                # NH, dataset = calculateNearestHigher_classic_mod(dataset, tileDict, outlierDeltaFactor*dc, delC, phoC, delM)    
-                dataset.to_csv('datasets/dataset1_correct_generated_c_noise_circles.csv')
                 dataset1 = findAndAssign_clusters_classic_fast(dataset, tileDict, delM)
 
-                dataset1.to_csv('D1_correct_generated_c.csv')
-                
-
-                h_score,c_score,v_score = homogeneity_completeness_v_measure(dataset1['clusterId'].values, dataset1['ClusterNumbers'].values)
-                #print(h_score, c_score, v_score)
+                h_score,c_score,v_score = energy_aware_homogeneity_completeness_v_measure(dataset1['clusterId'].values, dataset1['ClusterNumbers'].values, energy=dataset1['weight'].values)
+                print(h_score, c_score, v_score)
                 h_scores[noise_index, iter,n_sigma] = h_score
+                plt.rc('text', usetex=True)
 
-                sns.scatterplot(data=dataset1, x="x", y="y", hue="ClusterNumbers", palette="deep").set_title('Computed clusters')
+                plt.tick_params(axis='y',which='both', left=True,      # ticks along the bottom edge are off
+    right=False,         # ticks along the top edge are off
+    labelleft=False) # labels along the bottom edge are off)
+
+
+                sns.scatterplot(data=dataset1, x="x", y="y", hue="ClusterNumbers", palette="deep")
                 plt.legend([],[], frameon=False)
-                plt.ylim(-300,300)
-                plt.text(0,275, 'score: '+ str(round(h_score,2)))
-                plt.savefig(output_dir+'computed_clusters_'+str(noise_sizes[noise_index])+'_sigma_' + str(sigma)+'.png')
+                # plt.ylim(-200,200)
+                # plt.xlim(-200,200)
+                plt.text(-230,-230, '$\mathcal{F}_H= $'+ str(round(h_score,2)))
+                plt.savefig(output_dir+'computed_clusters_'+str(noise_sizes[noise_index])+'_sigma_' + str(sigma)+'.svg')
                 plt.close()
 
                 sns.scatterplot(data=dataset1, x="x", y="y", hue="clusterId", palette="deep").set_title('True clusters')
                 plt.legend([],[], frameon=False)
-                plt.savefig(output_dir+'true_clusters_'+str(noise_sizes[noise_index])+'_sigma_' + str(sigma)+'.png')
+                plt.savefig(output_dir+'true_clusters_'+str(noise_sizes[noise_index])+'_sigma_' + str(sigma)+'.svg')
                 plt.close()
 
         np.save(output_dir+'h_scores_noise_sigma_' + str(sigma) + '.npy', h_scores)
     else:
-        #print('Loading from file')
-        h_scores = np.load(output_dir+'h_scores_noise_sigma_' + str(sigma) + '.npy')          
-    plt.plot([i/n_samples[0] for i in noise_sizes], np.mean(h_scores[:,:,n_sigma], axis=1), label='homogeneity')
-    plt.fill_between([i/n_samples[0] for i in noise_sizes], np.mean(h_scores[:,:,n_sigma], axis=1)-np.std(h_scores[:,:,n_sigma], axis=1), np.mean(h_scores[:,:,n_sigma], axis=1)+np.std(h_scores[:,:,n_sigma], axis=1), alpha=0.2)
-    # plt.savefig(output_dir+'homogeneity_noise_sigma_'+str(sigma)+'.png')
-    # plt.close()
-plt.xlabel('No. of noise samples/No. of cluster samples')
-plt.ylabel('Homogeneity Score')
-plt.legend(sigmas)
-plt.savefig(output_dir+'homogeneity_noise_sigma.png')
+        print('Loading from file')
+        h_scores = np.load(output_dir+'h_scores_noise_sigma_' + str(sigma) + '.npy')
+    print(h_scores.shape)
+    plt.plot([i/n_samples[0] for i in noise_sizes], np.mean(h_scores[:,:,0], axis=1), label='homogeneity')
+    plt.fill_between([i/n_samples[0] for i in noise_sizes], np.mean(h_scores[:,:,0], axis=1)-np.std(h_scores[:,:,0], axis=1), np.mean(h_scores[:,:,0], axis=1)+np.std(h_scores[:,:,0], axis=1), alpha=0.2)
+
+plt.xlabel('$N_{N}/N_{C}$')
+plt.ylabel('$\mathcal{F}$')
+plt.legend(['$\sigma=$' + str(i) for i in sigmas])
+plt.savefig(output_dir+'homogeneity_noise_sigma.svg')
 plt.close()
-            # v_scores[noise_index, iter] = v_score
-    # print(h_scores)
-
-# print(h_scores)
-# print(c_scores)
-# print(v_scores)
-
-# np.save('h_scores_noise.npy', h_scores)
-# np.save('c_scores_noise.npy', c_scores)
-# np.save('v_scores_noise.npy', v_scores)
-
-# h_scores = np.load('h_scores_noise.npy')
-# c_scores = np.load('c_scores_noise.npy')
-# v_scores = np.load('v_scores_noise.npy')
-# print(h_scores.shape)
-# print(c_scores.shape)
-# print(v_scores.shape)
-    
-
-# plt.close()
-# plt.plot(noise_sizes, np.mean(c_scores, axis=1), label='completeness')
-# plt.fill_between(noise_sizes, np.mean(c_scores,axis=1)-np.std(c_scores, axis=1), np.mean(c_scores, axis=1)+np.std(c_scores, axis=1), alpha=0.2)
-# plt.savefig(data_dir+'completeness_noise.png')
-# plt.close()
-
-# plt.plot(noise_sizes, v_scores, label='v_measure')
-# plt.fill_between(noise_sizes, np.mean(v_scores, axis=1)-np.std(v_scores, axis=1), np.mean(v_scores, axis=1)+np.std(v_scores, axis=1), alpha=0.2)
-# plt.savefig(data_dir+'v_measure_noise.png')
-# plt.close()
-
-# a = np.array([h_scores, c_scores, v_scores])
-# np.save(data_dir+'all_scores_noise.npy', a)
 
 ft = time.time()
-#print('Time taken:', ft-it)
-
-
-
-    # wandb.log({
-    #     'homogeneity': h, 
-    #     'completeness': c,
-    #     'v_measure': v,
-    #     })
-# print(NH)
-# print(dataset1.head())
-# dataset1.to_csv('dataset1.csv')
-# print(NH==trueNh)
-# print(pauli_gen("I", 0, 2))
-
-
-####### TODOLIST
-#     # - calculate local density
-    # write quantum version of the function using one of two methods (grover -> one-point state, grover -> all-points state)
-
-    # - find nearest higher
-    # use grover -> one-point state (good because nearest higher is unique)       
-
-# wandb.agent(sweep_id, function=main)
-# fig = plt.figure()
-# plt.scatter(allX,allY)
-# plt.savefig('scatter_plot.pdf')
+print('Time taken:', ft-it)
